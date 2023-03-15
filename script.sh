5e8ba84e1d63d4d3ea937c6c5a62e42491ac7ea7
R.B. Boyer <rb@hashicorp.com>
2023-03-15 16:15:58 -0500

    foo

--- script.sh
+++ script.sh
@@ -9,11 +9,11 @@ cd "$(dirname "$0")"
 # If we're not on OS X, then error
 case $OSTYPE in
     darwin*)
-        HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask docker
-        /Applications/Docker.app/Contents/MacOS/Docker --unattended --install-privileged-components
-        open -a /Applications/Docker.app --args --unattended --accept-license
-        echo "We are waiting for Docker to be up and running. It can take over 2 minutes..."
-        while ! /Applications/Docker.app/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
+        # HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask docker
+        # /Applications/Docker.app/Contents/MacOS/Docker --unattended --install-privileged-components
+        # open -a /Applications/Docker.app --args --unattended --accept-license
+        # echo "We are waiting for Docker to be up and running. It can take over 2 minutes..."
+        # while ! /Applications/Docker.app/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
 
         echo "docker.local: $(host docker.local)"
 
